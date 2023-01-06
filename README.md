8e82d936fc1e8a9936a2f128b9e75e545020349b
Jonas Heinrich <onny@project-insanity.org>
2023-01-06 18:14:58 +0100

    use multiplatform dev shell

--- README.md
+++ README.md
@@ -1,13 +1,22 @@
 # iaroapp
 
+## Setup
+
+On MacOS
+
+```
+sh <(curl -L https://nixos.org/nix/install)
+# Restart terminal after successfull installation
+nix --extra-experimental-features "nix-command flakes" run git clone https://git.project-insanity.org/onny/iaroapp.git
+cd iaroapp
+nix --extra-experimental-features "nix-command flakes" develop
+```
+
 ## Run local server
 
-On NixOS
 
 ```
 nix develop
 python manage.py migrate
 python manage.py runserver
 ```
-
-On MacOS
--- flake.nix
+++ flake.nix
@@ -1,17 +1,21 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        packages = with pkgs; [
-          python3Packages.django
-          python3Packages.dj-database-url
-          python3Packages.whitenoise
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            python3Packages.django
+            python3Packages.dj-database-url
+            python3Packages.whitenoise
+          ];
+        };
+      });
 }
