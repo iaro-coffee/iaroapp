65c57b20609cfc7d6b1d47a6f6e1ed68d202114f
Jonas Heinrich <onny@project-insanity.org>
2023-02-08 16:36:43 +0100

    synology hosting update

--- iaroapp/settings.py
+++ iaroapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://*.railway.app','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['http://app.iaro.co','https://*.127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
