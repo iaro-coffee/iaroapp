8a6bf2a48f660ddf0f956ef1a19cf1907507e7af
Jonas Heinrich <onny@project-insanity.org>
2023-02-18 18:38:38 +0100

    add pieces to unit list

---
+++ inventory/migrations/0007_unitsaddition.py
@@ -0,0 +1,15 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+
+    def create_units(apps, schema_editor):
+        unit = apps.get_model('inventory', 'Units')
+        unit.objects.create(name='Stück')
+
+    dependencies = [
+        ('inventory', '0006_product_modified_date'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_units)
+    ]
