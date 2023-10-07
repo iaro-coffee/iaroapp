df8eda1d3e1bc1e6178e6c0ec362a342aecbc530
Raphael Fritz <raphael.fritz@articlett.de>
2023-10-07 19:27:41 +0200

    fix: tips reporting

--- tips/views.py
+++ tips/views.py
@@ -5,7 +5,7 @@ from iaroapp.cron import assignTips
 from ratings.models import EmployeeRating
 # Create your views here.
 
-from .models import Tip
+from .models import Tip, AssignedTip
 
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
@@ -58,91 +58,28 @@ def check_admin(user):
 
 @user_passes_test(check_admin)
 def evaluation(request):
-
-    User = get_user_model()
-    users = User.objects.all()
-
-    # Calculate chart
-    chartLabels = []
-    dayNow = datetime.datetime.now()
-    for lastDays in range(0,7):
-        lastDay = dayNow - datetime.timedelta(days=6-lastDays)
-        chartLabels.append(lastDay.strftime("%A"))
-    chartValues = []
-    allTips = Tip.objects.all()
-    for lastDays in range(0,7):
-        thisDay = dayNow - datetime.timedelta(days=6-lastDays)
-        thisDay = thisDay.strftime("%Y-%m-%d")
-        amountToday = 0
-        for tip in allTips:
-            tip = model_to_dict(tip)
-            tipDate = tip['date'].date()
-            if str(tipDate) == str(thisDay):
-                amountToday += float(tip['amount'])
-        chartValues.append(amountToday)
-
-    # Calculate evaluation
-    evaluation = []
-    result = Tip.objects.all()
-    for user in users:
-        userDict = {}
-        userDict['user_id'] = user.id
-        userDict['user_name'] = user.get_username()
-        userDict['user_firstname'] = user.first_name
-        userDict['user_lastname'] = user.last_name
-        userDict['amountToday'] = 0
-        userDict['amountThisMonth'] = 0
-        userDict['amountLastMonth'] = 0
-        for tip in result:
-            tip = model_to_dict(tip)
-            if user.id == tip['user']:
-
-                tipDate = tip['date'].date()
-                today = datetime.datetime.today().date()
-                first = today.replace(day=1)
-                lastMonth = first - datetime.timedelta(days=1)
-
-                if today == tipDate:
-                    userDict['amountToday'] += float(tip['amount'])
-
-                if strip_day(today) == strip_day(tipDate):
-                    userDict['amountThisMonth'] += float(tip['amount'])
-
-                if strip_day(lastMonth) == strip_day(tipDate):
-                    userDict['amountLastMonth'] += float(tip['amount'])
-        evaluation.append(userDict)
-
-    # Calculate evaluation
-    latestTips = []
-    result = Tip.objects.all()
-    for tip in result:
-        entry = {}
-        tip = model_to_dict(tip)
-        if tip['amount'] > 0 or tip['amount'] < 0:
-            entry['amount'] = tip['amount']
-            entry['date'] = tip['date'].strftime("%d.%m.%Y %H:%M") + " Uhr"
-            user = User.objects.get(id=int(tip['user']))
-            if (user.first_name and user.last_name):
-                username = user.first_name + " " + user.last_name
-            else: 
-                username = user
-            entry['user'] = username
-            latestTips.append(entry)
-
-    # Reverse list, having newest tips on top
-    latestTips = list(reversed(latestTips))
-    # Limit recent tips list to 20 entries
-    latestTips = latestTips[:20]
+    assignedTips = AssignedTip.objects.filter(date__month__gte=datetime.datetime.now().strftime("%m"))
+    # {date: date, assignedTips: [{name, minutes, amount}], sumHours: sumHours, sumAmount: sumAmount}
+    items = []
+    for i in range(1, 32):
+        tips = []
+        sumHours = 0
+        sumAmount = 0
+        for tip in assignedTips:
+            if tip.date.date() == datetime.datetime.now().replace(day=i).date():
+                tips.append({'name': tip.user.username, 'hours': tip.minutes / 60, 'amount': tip.amount})
+                sumHours += tip.minutes/60
+                sumAmount += tip.amount
+        if tips:
+            items.insert(0,{'id': i, 'date': datetime.datetime.now().replace(day=i).date(), 'assignedTips': tips, 'sumHours': sumHours, 'sumAmount': sumAmount})
+    print(items)
 
     return render(
         request,
         'evaluation.html',
         context={
-            'users': users,
-            'evaluation': evaluation,
-            'chartLabels': chartLabels,
-            'chartValues': chartValues,
-            'latestTips': latestTips
+            'today': datetime.datetime.now().date(),
+            'items': items,
         },
     )
 
