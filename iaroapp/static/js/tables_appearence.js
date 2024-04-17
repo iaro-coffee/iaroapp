d6dfde3df135b51ca6b14d35d4893bf055a36025
Ayshyama@gmail.com <Ayshyama@gmail.com>
2024-04-17 14:17:32 +0200

    adjusted elements visability for dark-theme, changed root colors to stylepaper

--- iaroapp/static/js/tables_appearence.js
+++ iaroapp/static/js/tables_appearence.js
@@ -2,23 +2,32 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Background for done tasks
+    var body = document.body;
     var checkboxes = document.querySelectorAll('input[type="checkbox"][name^="done_"]');
     var rows = document.querySelectorAll('tbody tr');
 
+    // Func to update row bg-color based on checkbox state
+    function updateRowColor(checkbox) {
+        var row = checkbox.closest('tr');
+        var checkboxWrapper = checkbox.closest('.checkbox-wrapper');
+        var isDarkPage = body.classList.contains('dark-page');
+
+        if (checkboxWrapper && checkbox.checked) {
+            row.style.backgroundColor = isDarkPage ? "var(--bs-success-dark)" : "var(--bs-success)";
+        } else {
+            row.style.backgroundColor = '';
+        }
+    }
+
     checkboxes.forEach(function (checkbox) {
         checkbox.addEventListener('change', function () {
-            var row = this.closest('tr');
-            var checkboxWrapper = this.closest('.checkbox-wrapper');
-            if (checkboxWrapper && this.checked) {
-                row.style.backgroundColor = "#e7e7e9";
-            } else {
-                row.style.backgroundColor = '';
-            }
+            updateRowColor(this);
         });
 
-        checkbox.dispatchEvent(new Event('change'));
+        // Initially trigger
+        updateRowColor(checkbox);
     });
+
 
     rows.forEach(row => {
         row.addEventListener('click', function (event) {
@@ -31,4 +40,19 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     });
+
+    // Observer to detect class changes on the body
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            if (mutation.attributeName === "class") {
+                checkboxes.forEach(function (checkbox) {
+                    updateRowColor(checkbox);
+                });
+            }
+        });
+    });
+    var config = { attributes: true };
+
+    // Start observing
+    observer.observe(body, config);
 });
