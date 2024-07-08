cbb88c5033fa13f2a543a7762311026038eff6da
igor-deb-pc <ayshyama@gmail.com>
2024-07-08 22:32:06 +0200

    hided notes in admin panel

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-
-from .models import Note
-
-
-@admin.register(Note)
-class NoteAdmin(admin.ModelAdmin):
-    list_display = ("sender", "content", "timestamp")
-    search_fields = ("sender__username", "content")
+# from django.contrib import admin
+#
+# from .models import Note
+#
+#
+# @admin.register(Note)
+# class NoteAdmin(admin.ModelAdmin):
+#     list_display = ("sender", "content", "timestamp")
+#     search_fields = ("sender__username", "content")
